c836d8015f44fdaada995a57c8372b9a124cf9cb
asense <senseandrey@gmail.com>
2018-11-02 01:44:58 +0200

    All includes and namespaces

---
+++ Header.h
@@ -0,0 +1,32 @@
+#pragma once
+#include<iostream>
+#include<conio.h>
+#include<cstdlib>
+#include<ctime>
+#include <conio.h>
+#include <Windows.h>
+#include <iomanip>
+
+
+
+
+#define MAXROW 13
+#define MAXCOL 9
+#define numofbombs 10
+
+
+using namespace std;
+
+
+//Prototypu usih funkciy
+int minearr[MAXROW][MAXCOL];
+bool manage_mine[MAXROW][MAXCOL];
+void intro();
+void init_mine();
+void prep_mine(int tmpr, int tmpc);
+void openingemptybox(int rownum, int colnum);
+int openedboxnum();
+void show_mine();
+void game_over();
+bool test_input(int rowno, int colno);
+void menu();
